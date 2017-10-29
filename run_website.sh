a784568ed000286dba73a30b40a1edba61736c9c
Brandon Minor <brandon@occipital.com>
2017-10-29 11:01:17 -0600

    Add a small setup script

---
+++ run_website.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get RVM, install ruby, and switch to the latest verions
+$ \curl -L https://get.rvm.io | bash -s stable --rails --autolibs=enabled # Or, --ruby=1.9.3
+rvm install 2.4
+rvm use 2.4
+
+# Grab our necessary Gems
+gem install bundler
+bundle install
+
+# jekyll watches automatically for changes, so this should do it
+jekyll serve
